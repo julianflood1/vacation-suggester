83fead3754b58dbb7bf8fa8d170dd4b44fa79813
Julian Flood <Julian@Julians-MacBook-Pro.local>
2017-02-24 16:48:59 -0800

    complete majority of js, including if then statements, and hide survey upon submission

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,30 @@ $(document).ready(function() {
     event.preventDefault();
 
     var nameInput = $('#name').val();
-    var favorite-seasonInput = $('#favorite-season').val();
-    var bedtimeInput = $('#bedtime').val();
-    var favorite-foodInput = $('#favorite-food').val();
-    var past-timeInput = $('#past-time').val();
-    var totalInput = (favorite-seasonInput + bedtimeInput + favorite-foodInput + past-timeInput);
+    var favoriteSeasonInput = parseInt($('#favorite-season').val());
+    var bedtimeInput = parseInt($('#bedtime').val());
+    var favoriteFoodInput = parseInt($('#favorite-food').val());
+    var pastTimeInput = parseInt($('#past-time').val());
+    var totalInput = (favoriteSeasonInput + bedtimeInput + favoriteFoodInput + pastTimeInput);
 
     $('.name').text(nameInput);
+
+    $('#portland').hide();
+    $('#sanfrancisco').hide();
+    $('#miami').hide();
+    $('#lasvegas').hide();
+    $('#survey').hide();
+
+    if((totalInput >= 40) && (totalInput <= 70)) {
+      $('#portland').show();
+    } else if ((totalInput >= 80) && (totalInput <= 110 )) {
+      $('#sanfrancisco').show();
+    } else if ((totalInput >= 120) && (totalInput <= 150 )) {
+      $('#miami').show();
+    } else if ((totalInput >= 160) && (totalInput <= 5000 )) {
+      $('#lasvegas').show();
+    }  else (alert("Please completely fill out survey!"));
+
 
 
   });
